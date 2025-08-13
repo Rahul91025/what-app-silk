bb1c941ab7920c2d14b76f14b00c74c510fbba86
Rahul Gupta <rahulguptacsit@gmail.com>
2025-08-13 15:34:34 +0530

    first commit

---
+++ src/components/Sidebar/Sidebar.jsx
@@ -0,0 +1,78 @@
+import React, { useEffect, useState } from "react";
+import "./Sidebar.css";
+import { Avatar, IconButton } from "@material-ui/core";
+import DonutLargeIcon from "@material-ui/icons/DonutLarge";
+import ChatIcon from "@material-ui/icons/Chat";
+import MoreVertIcon from "@material-ui/icons/MoreVert";
+import { SidebarChat } from "../SidebarChat/SidebarChat";
+import db from "../../firebase";
+import { Search } from "../Search/Search";
+
+export const Sidebar = () => {
+  const [rooms, setRooms] = useState([]);
+
+  const getRooms = () => {
+    const unsubscribe = db.collection("rooms").onSnapshot((snapshot) =>
+      setRooms(
+        snapshot.docs.map((doc) => ({
+          id: doc.id,
+          data: doc.data(),
+        }))
+      )
+    );
+
+    return () => {
+      unsubscribe();
+    };
+  };
+
+  useEffect(() => {
+    getRooms();
+  }, []);
+
+  const deleteRoom = (id) => {
+    const objectRef = db.collection("rooms").doc(id);
+    objectRef
+      .delete()
+      .then(() => {
+        console.log("Object deleted successfully");
+      })
+      .catch((error) => {
+        console.error("Error deleting object:", error);
+      });
+  };
+  return (
+    <div className="sidebar">
+      <div className="sidebar__header">
+        <Avatar />
+        <div className="sidebar__headerRight">
+          <IconButton>
+            <DonutLargeIcon />
+          </IconButton>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </div>
+      </div>
+      <div className="sidebar__search">
+        <Search rooms={rooms} setRooms={setRooms} getRooms={getRooms} />
+      </div>
+      <div className="sidebar__chat">
+        <SidebarChat addNewChat />
+        {rooms.map((room) => {
+          return (
+            <SidebarChat
+              key={room.id}
+              id={room.id}
+              name={room.data.name}
+              deleteRoom={deleteRoom}
+            />
+          );
+        })}
+      </div>
+    </div>
+  );
+};
